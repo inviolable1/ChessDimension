361cdf705d090e4755cda710cff5521f4e9dd84c
Tim Wee <timwee999@gmail.com>
2013-03-04 21:38:05 +1100

    Added Migration Controller, 1st Migration
    *Enabled migrations in config\migration.php
    *Remember to disable migrations when done with migrations (for security)

---
+++ application/controllers/migrate.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Migrate extends CI_Controller {
+ 
+    public function __construct(){
+ 
+        parent::__construct();
+        $this->load->library('migration');
+ 
+    }
+ 
+    public function index(){
+	
+		echo 'lol';
+		
+        //this code means we're always going to get the latest migrations, that's why we didn't need to worry about migration_version in the migration config.
+        if(!$this->migration->latest()){	//this $this->migration->latest() is actually a command to update (UP function), if it fails to update, then !$this = TRUE, then return error
+            show_error($this->migration->error_string());
+		}
+ 
+    }
+ 
+}
