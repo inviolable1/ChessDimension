202d59cd97bdfd9239c3e83dda5e5509c6f18f0a
Tim Wee <timwee999@gmail.com>
2013-03-04 22:19:27 +1100

    Added 'Revert' function to Migrate Controller, Created 2nd Migration
    *carried out 2nd Migration - dropped irrelevant tables
    **Note that the test table 'logindata' is still in database - this is
    for reference only, should be removed later.

--- application/controllers/migrate.php
+++ application/controllers/migrate.php
@@ -11,7 +11,7 @@ class Migrate extends CI_Controller {
  
     public function index(){
 	
-		echo 'lol';
+		echo 'Latest Migration';
 		
         //this code means we're always going to get the latest migrations, that's why we didn't need to worry about migration_version in the migration config.
         if(!$this->migration->latest()){	//this $this->migration->latest() is actually a command to update (UP function), if it fails to update, then !$this = TRUE, then return error
@@ -19,5 +19,15 @@ class Migrate extends CI_Controller {
 		}
  
     }
+	
+	public function revert($num){
+		//e.g. if we use revert(4) when our current version is 7. We will revert back to version 4. 
+		//This will carry out down(5),down(6),down(7).
+		//**NOTE: we will not want revert in our production- otherwise someone could revert your database schema!
+		if(!$this->migration->version($num)){
+			show_error($this->migration->error_string());
+		}
+	
+	}
  
 }
