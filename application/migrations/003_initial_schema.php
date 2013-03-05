1cf564151a3fdf5494a2754887ac23244a7836e0
Tim Wee <timwee999@gmail.com>
2013-03-05 01:12:15 -0500

    Added Initial Schema (partly done)

---
+++ application/migrations/003_initial_schema.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Migration_initial_schema extends CI_Migration {
+    
+    public function up(){
+ 
+        //1) Table 'users' - for key user data. (Other less important user data like profile/description/image will be put in another table.)
+        $this->dbforge->add_field('id');
+        
+        $this->dbforge->add_field(
+            array(
+                'FirstName' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '40',
+                ),
+                'LastName' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '40',
+                ),
+                'Email' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '70'
+                ),
+                'Username' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '30',
+                ),
+                'Password' => array(
+                    'type' => 'VARCHAR',
+                    'constraint' => '256',  //because we are encrypting password using hash
+                ),
+            )
+        );
+                    
+        $this->dbforge->create_table('users');
+            
+        //2) Table 'groups' - to segment users into user groups.
+        $this->dbforge->add_field('id');    //ask: how do i make only a few group IDs - for each corresponding group name? then many users.id in one group ID. This current id will auto increment- not what I want.
+        
+        $this->dbforge->add_field(
+            array(
+                'Users.id' => array(
+                    'type' => 'INT',
+                    'constraint' => 9,
+                ),
+                'PermissionGroup' => array( //Segments users into Admin, VIP Member, Premium Member, Member, Guest.
+                    'type' => 'VARCHAR',
+                    'constraint' => '20',
+                ),
+                'RankGroup' => array(   //like Playchess.com or Stack Overflow - different rights for Rook/Knight/Queen status.
+                    'type' => 'VARCHAR',
+                    'constraint' => '20',
+                ),
+            )             
+        );
+ 
+        $this->dbforge->create_table('users');
+    
+    }
+ 
+    public function down(){
+ 
+        $this->dbforge->drop_table('users');
+        $this->dbforge->drop_table('groups');
+ 
+    }
+ 
+}
