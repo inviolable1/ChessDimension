45d6f25ee0135be6df89cfc55996ef4204cff9c2
Tim Wee <timwee999@gmail.com>
2013-03-05 21:39:58 -0500

    Config change to cater for Cloud9

--- application/migrations/003_initial_schema.php
+++ application/migrations/003_initial_schema.php
@@ -4,7 +4,21 @@ class Migration_initial_schema extends CI_Migration {
     
     public function up(){
  
-        //1) Table 'users' - for key user data. (Other less important user data like profile/description/image will be put in another table.)
+        //1) Table 'accessgroups' - segment users by access status (ABSTRACT of users)
+        $this->dbforge->add_field('id');   
+        
+        $this->dbforge->add_field(
+            array(
+                'PermissionGroup' => array( //Segments users into Admin, VIP Member, Premium Member, Member, Guest.
+                    'type' => 'VARCHAR',
+                    'constraint' => '20',
+                ),
+            )             
+        );
+ 
+        $this->dbforge->create_table('accessgroups');    
+ 
+        //2) Table 'users' - for key user data. (Other less important user data like profile/description/image will be put in another table.)
         $this->dbforge->add_field('id');
         
         $this->dbforge->add_field(
@@ -29,41 +43,21 @@ class Migration_initial_schema extends CI_Migration {
                     'type' => 'VARCHAR',
                     'constraint' => '256',  //because we are encrypting password using hash
                 ),
+                'AccessGroupsId' => array(
+                    'type' => 'INT',
+                    'constraint' => 9,                    
+                ),
             )
         );
                     
         $this->dbforge->create_table('users');
             
-        //2) Table 'usergroups' - to segment users into user groups.
-        $this->dbforge->add_field('id');    //ask: how do i make only a few group IDs - for each corresponding group name? then many users.id in one group ID. This current id will auto increment- not what I want.
-        
-        $this->dbforge->add_field(
-            array(
-                'Users.id' => array(
-                    'type' => 'INT',
-                    'constraint' => 9,
-                ),
-                'PermissionGroup' => array( //Segments users into Admin, VIP Member, Premium Member, Member, Guest.
-                    'type' => 'VARCHAR',
-                    'constraint' => '20',
-                ),
-                'RankGroup' => array(   //like Playchess.com or Stack Overflow - different rights for Rook/Knight/Queen status.
-                    'type' => 'VARCHAR',
-                    'constraint' => '20',
-                ),
-            )             
-        );
- 
-        $this->dbforge->create_table('usergroups');
-		
-		
-    
     }
  
     public function down(){
  
         $this->dbforge->drop_table('users');
-        $this->dbforge->drop_table('groups');
+        $this->dbforge->drop_table('accessgroups');
  
     }
  
