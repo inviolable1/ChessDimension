8986e7859dff539642cc67632b372c91c861bf28
Tim Wee <timwee999@gmail.com>
2013-03-06 19:40:38 +1100

    Setup IonAuth
    *Autoload IonAuth libraries
    *Change IonAuth config
    *Change IonAuth password in migrations/004_add_ionauth.php and in the
    PHPMyAdmin Database, per instructions in IonAuth Config

--- application/config/ion_auth.php
+++ application/config/ion_auth.php
@@ -66,7 +66,7 @@ $config['join']['groups'] = 'group_id';
  | Becareful how high you set max_rounds, I would do your own testing on how long it takes
  | to encrypt with x rounds.
  */
-$config['hash_method']    = 'sha1';	// IMPORTANT: Make sure this is set to either sha1 or bcrypt
+$config['hash_method']    = 'bcrypt';	// IMPORTANT: Make sure this is set to either sha1 or bcrypt
 $config['default_rounds'] = 8;		// This does not apply if random_rounds is set to true
 $config['random_rounds']  = FALSE;
 $config['min_rounds']     = 5;
@@ -81,21 +81,21 @@ $config['max_rounds']     = 9;
  | The controller should check this function and act
  | appropriately. If this variable set to 0, there is no maximum.
  */
-$config['site_title']                 = "Example.com";       // Site Title, example.com
-$config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
+$config['site_title']                 = "Commonprogramming";       // Site Title, example.com
+$config['admin_email']                = "timwee999@gmail.com"; // Admin Email, admin@example.com
 $config['default_group']              = 'members';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             // A database column which is used to login with
 $config['min_password_length']        = 8;                   // Minimum Required Length of Password
 $config['max_password_length']        = 20;                  // Maximum Allowed Length of Password
-$config['email_activation']           = FALSE;               // Email Activation for registration
+$config['email_activation']           = FALSE;               // Email Activation for registration	 - FALSE for development
 $config['manual_activation']          = FALSE;               // Manual Activation for registration
 $config['remember_users']             = TRUE;                // Allow users to be remembered and enable auto-login
 $config['user_expire']                = 86500;               // How long to remember the user (seconds). Set to zero for no expiration
-$config['user_extend_on_login']       = FALSE;               // Extend the users cookies everytime they auto-login
-$config['track_login_attempts']       = FALSE;               // Track the number of failed login attempts for each user or ip.
+$config['user_extend_on_login']       = TRUE;               // Extend the users cookies everytime they auto-login
+$config['track_login_attempts']       = TRUE;               // Track the number of failed login attempts for each user or ip.
 $config['maximum_login_attempts']     = 3;                   // The maximum number of failed login attempts.
-$config['lockout_time']               = 600;                 // The number of miliseconds to lockout an account due to exceeded attempts
+$config['lockout_time']               = 10000;                 // The number of miliseconds to lockout an account due to exceeded attempts
 $config['forgot_password_expiration'] = 0;                   // The number of miliseconds after which a forgot password request will expire. If set to 0, forgot password requests will not expire.
 
 
@@ -107,7 +107,7 @@ $config['forgot_password_expiration'] = 0;                   // The number of mi
  | 	  'file' = Use the default CI config or use from a config file
  | 	  array  = Manually set your email config settings
  */
-$config['use_ci_email'] = FALSE; // Send Email using the builtin CI email class, if false it will return the code and the identity
+$config['use_ci_email'] = TRUE; // Send Email using the builtin CI email class, if false it will return the code and the identity
 $config['email_config'] = array(
 	'mailtype' => 'html',
 );
--- application/migrations/004_add_ionauth.php
+++ application/migrations/004_add_ionauth.php
@@ -142,7 +142,7 @@ class Migration_add_ionauth extends CI_Migration {
 			'id' => '1',
 			'ip_address' => 0x7f000001,
 			'username' => 'administrator',
-			'password' => '59beecdf7fc966e2f17fd8f65a4a9aeb09d4a3d4',
+			'password' => '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36',
 			'salt' => '9462e8eee0',
 			'email' => 'admin@admin.com',
 			'activation_code' => '',
