ba16f36d33e255a06dd1015df4cc248380b8f18d
Tim Wee <timwee999@gmail.com>
2013-03-07 20:16:28 +1100

    Config, set up new site_config file, add layout

---
+++ application/config/site_config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+//abstract the commonly shared view data to the site_config.php file that is being autoloaded
+//go to autoload, and add site_config.php
+//can refer to it from $this->config->item('view_data')
+
+
+
+$config['chessdimension'] = array(
+	'view_data' => array(),
+);
